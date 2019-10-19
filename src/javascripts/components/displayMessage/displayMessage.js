f23bf0855c5df64d335ba04896dab8673cd326d7
John-Ryan-Johnson <john4johnkids@gmail.com>
2019-10-19 12:40:24 -0500

    moved button

--- src/javascripts/components/displayMessage/displayMessage.js
+++ src/javascripts/components/displayMessage/displayMessage.js
@@ -9,21 +9,23 @@ const displayMessageCard = (arr) => {
       domString += `
 
     <div class="card-container" id="${arr[i].id}">
+    <button id="even" class="close d-flex justify-content-end">x</button>
     <h5>${arr[i].userName}</h5>
     <img src="${arr[i].userIcon}" alt="Photo">
     <p>${arr[i].messageContent}</p>
     <span class="time-right">${arr[i].timeStamp}</span>
-    <button id="even" class="close d-flex justify-content-end">x</button>
+    
     </div>
     `;
     } else {
       domString += `
         <div class="card-container-darker" id="${arr[i].id}">
+        <button id="odd" class="close d-flex justify-content-start">x</button>
         <h5 align="right">${arr[i].userName}</h5>
         <img src="${arr[i].userIcon}" alt="Photo" class="right">
         <p align="right">${arr[i].messageContent}</p>
         <span class="time-left">${arr[i].timeStamp}</span>
-        <button id="odd" class="close d-flex justify-content-start">x</button>
+        
         </div>
         `;
     }
