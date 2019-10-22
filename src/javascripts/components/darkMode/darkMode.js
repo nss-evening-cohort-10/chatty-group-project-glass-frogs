1439460221388f5354ba4540a9d73d9a269ef8f2
Isis Christopher <isischristopher@gmail.com>
2019-10-22 14:42:06 -0500

    darkMode theme not fixed yet

--- src/javascripts/components/darkMode/darkMode.js
+++ src/javascripts/components/darkMode/darkMode.js
@@ -1,10 +1,12 @@
 import $ from 'jquery';
 import '../../../styles/themeMaps.scss';
 
+
 const darkMode = () => {
   const root = $('#app-root');
   root.toggleClass('theme-light');
   root.toggleClass('theme-dark');
+  // $('#app-root').toggleClass('theme-dark');
 };
 
 const makeDark = () => {
