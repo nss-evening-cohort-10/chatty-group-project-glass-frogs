35be56e2ae2365c491481a85b66e09a1fb85182a
Isis Christopher <isischristopher@gmail.com>
2019-10-22 17:11:30 -0500

    darkMode and emojis

---
+++ src/javascripts/components/emojis/emojis.js
@@ -0,0 +1,27 @@
+
+import $ from 'jquery';
+
+const emojis = [
+  {
+    name: 'smiley face',
+    unicode: 'U+1F970',
+    emoji: '🥰',
+    userInput: ':smile:',
+  },
+  {
+    name: 'frowny face',
+    unicode: 'U+1F97A',
+    emoji: '🥺',
+    userInput: ':sad:',
+  },
+];
+
+const emojiInput = () => {
+  $('#send-button').on('click', () => {
+    console.error(emojis[0].emoji);
+    $("p:contains('smile')").replaceWith(emojis[0].emoji);
+    $("p:contains('sad')").replaceWith(emojis[1].emoji);
+  });
+};
+
+export default { emojiInput };
